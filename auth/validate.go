1747483bcd54680c053d31a8ee23bf75d0235c65
danil-bragin <danil@iota.uz>
2025-06-05 18:10:35 +0500

    add statuses in const & validate signature

---
+++ auth/validate.go
@@ -0,0 +1,23 @@
+﻿package auth
+
+import (
+	"crypto/sha1"
+	"crypto/subtle"
+	"encoding/hex"
+)
+
+func sha1Bytes(input string) [sha1.Size]byte {
+	return sha1.Sum([]byte(input))
+}
+
+func ValidateSignature(expectedHex, hashedMerchantSecret, octoPaymentUUID, status string) bool {
+	data := hashedMerchantSecret + octoPaymentUUID + status
+	actualHash := sha1Bytes(data)
+
+	expectedBytes, err := hex.DecodeString(expectedHex)
+	if err != nil || len(expectedBytes) != sha1.Size {
+		return false
+	}
+
+	return subtle.ConstantTimeCompare(actualHash[:], expectedBytes) == 1
+}
---
+++ constants.go
@@ -0,0 +1,11 @@
+﻿package octoapi
+
+const (
+	CreatedStatus           = "created"
+	CancelledStatus         = "cancelled"
+	WaitUserActionStatus    = "wait_user_action"
+	WaitingForCaptureStatus = "waiting_for_capture"
+	Capture                 = "capture"
+	SucceededStatus         = "succeeded"
+	FailedStatus            = "failed"
+)
